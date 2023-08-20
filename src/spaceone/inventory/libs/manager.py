75f28282c66891cad067c9961a31312006f4de5f
kiku99 <elsd0326@gmail.com>
2023-08-20 16:12:10 +0900

    add : manager.py, resource.py, server_manager.py

---
+++ src/spaceone/inventory/libs/manager.py
@@ -0,0 +1,39 @@
+import json
+import logging
+from spaceone.core.manager import BaseManager
+from spaceone.inventory.libs.schema.resource import ErrorResourceResponse
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class NCloudManager(BaseManager):
+    connector_name = None
+
+    ## 수정 필요
+    def verify(self, options, secret_data, **kwargs):
+        """ Check collector's status.
+        """
+        connector = self.locator.get_connector(self.connector_name, secret_data=secret_data)
+        connector.verify()
+
+    def collect_resources(self, **kwargs) -> list:
+        try:
+            connector = self.locator.get_connector(self.connector_name, **kwargs)
+            return connector.collect_data()
+        except Exception as e:
+            _LOGGER.error(f'[collect_resources] {e}')
+
+            if type(e) is dict:
+                return [
+                    ErrorResourceResponse(
+                        {'message': json.dumps(e),
+                            'resource': {'cloud_service_group': connector.cloud_service_group,
+                            'cloud_service_type': connector.cloud_service_type}}
+                    )]
+            else:
+                return [
+                    ErrorResourceResponse(
+                        {'message': str(e),
+                            'resource': {'cloud_service_group': connector.cloud_service_group,
+                            'cloud_service_type': connector.cloud_service_type}}
+                    )]
---
+++ src/spaceone/inventory/manager/server/server_manager.py
@@ -0,0 +1,4 @@
+from spaceone.inventory.libs.manager import NCloudManager
+
+class ServerConnectorManager(NCloudManager):
+    connector_name = 'ServerConnector'
