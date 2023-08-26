dc44bd7e2591f1cff276fc85e528394a8742b8a4
kiku99 <elsd0326@gmail.com>
2023-08-26 18:57:04 +0900

    edit : cloud_service.py, data.py[optimization]

--- src/spaceone/inventory/model/compute/server/data.py
+++ src/spaceone/inventory/model/compute/server/data.py
@@ -230,21 +230,20 @@ class AutoScaler(Model):
     instance_group = ModelType(InstanceGroup, serialize_when_none=False)
 
 
-# Server = compute, google_cloud, hardware, os, server,
-# class Compute(Model):
-#     keypair = StringType(default="")
-#     public_ip_address = StringType()
-#     az = StringType()
-#     instance_id = StringType()
-#     instance_name = StringType(default='')
-#     instance_state = StringType(choices=(
-#         'PROVISIONING', 'STAGING', 'RUNNING', 'STOPPING', 'REPAIRING', 'SUSPENDING', 'SUSPENDED', 'TERMINATED'))
-#     instance_type = StringType()
-#     account = StringType()
-#     image = StringType()
-#     launched_at = DateTimeType()
-#     security_groups = ListType(StringType, default=[])
-#     tags = DictType(StringType, default={})
+class Compute(Model):
+    keypair = StringType(default="")
+    public_ip_address = StringType()
+    az = StringType()
+    instance_id = StringType()
+    instance_name = StringType(default='')
+    instance_state = StringType(choices=(
+        'PROVISIONING', 'STAGING', 'RUNNING', 'STOPPING', 'REPAIRING', 'SUSPENDING', 'SUSPENDED', 'TERMINATED'))
+    instance_type = StringType()
+    account = StringType()
+    image = StringType()
+    launched_at = DateTimeType()
+    security_groups = ListType(StringType, default=[])
+    tags = DictType(StringType, default={})
 
 
 class Scheduling(Model):
