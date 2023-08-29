ddc9c13a871c9e5eb630fb1d23a07bd4b6253ad9
kiku99 <elsd0326@gmail.com>
2023-08-29 20:24:11 +0900

    edit : connector.py, test_cloudservice_api.py

--- test/api/test_cloudservice_api.py
+++ test/api/test_cloudservice_api.py
@@ -5,15 +5,15 @@ import unittest
 from spaceone.core.unittest.runner import RichTestRunner
 from spaceone.tester import TestCase, print_json
 
-NAVER_APPLICATION_CREDENTIALS_PATH = os.environ.get('NAVER_APPLICATION_CREDENTIALS', None)
+AKI = os.environ.get('NCLOUD_ACCESS_KEY_ID', None)
+SK = os.environ.get('NCLOUD_SECRET_KEY', None)
 
-if NAVER_APPLICATION_CREDENTIALS_PATH is None:
+if AKI is None or SK is None:
     print("""
         ##################################################
         # ERROR 
         #
         # Configure your NCloud credential first for test
-        # https://console.cloud.google.com/apis/credentials
 
         ##################################################
         example)
@@ -23,13 +23,18 @@ if NAVER_APPLICATION_CREDENTIALS_PATH is None:
     exit
 
 
-def _get_credentials():
-    with open(NAVER_APPLICATION_CREDENTIALS_PATH) as json_file:
-        json_data = json.load(json_file)
-        return json_data
+# def _get_credentials():
+#     with open(NAVER_APPLICATION_CREDENTIALS_PATH) as json_file:
+#         json_data = json.load(json_file)
+#         return json_data
 
 
 class TestCollector(TestCase):
+
+    secret_data = {
+        'ncloud_access_key_id': AKI,
+        'ncloud_secret_key': SK
+    }
 
     def test_init(self):
         v_info = self.inventory.Collector.init({'options': {}})
@@ -37,13 +42,11 @@ class TestCollector(TestCase):
 
     def test_verify(self):
         options = {}
-        secret_data = _get_credentials()
-        v_info = self.inventory.Collector.verify({'options': options, 'secret_data': secret_data})
+        v_info = self.inventory.Collector.verify({'options': options, 'secret_data': self.secret_data})
         print_json(v_info)
 
     def test_collect(self):
-        secret_data = _get_credentials()
-        print(secret_data)
+        print(self.secret_data)
         '''
         Options can be selected
         options = {"cloud_service_types": ["SQLWorkspace"]}
@@ -62,7 +65,7 @@ class TestCollector(TestCase):
 
         resource_stream = self.inventory.Collector.collect({
             'options': options,
-            'secret_data': secret_data,
+            'secret_data': self.secret_data,
             'filter': filter})
 
         print(resource_stream)
