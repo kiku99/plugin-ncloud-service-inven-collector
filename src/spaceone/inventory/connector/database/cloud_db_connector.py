2affd26e0984f72dcc300a8dfd2603b88a3a1ea4
김나영` <knyg99@naver.com>
2023-10-08 14:12:35 +0900

    feat: add clouddb connector 함수
    clouddb connector에서 함수 추가
    
    ref: #27

--- src/spaceone/inventory/connector/database/cloud_db_connector.py
+++ src/spaceone/inventory/connector/database/cloud_db_connector.py
@@ -23,7 +23,7 @@ class CloudDBConnector(NaverCloudConnector):
                 instance_list.append(instance)
 
         except ApiException as e:
-            print("Exception when calling V2Api->get_server_instance_list: %s\n" % e)
+            print("Exception when calling V2Api->get_cloud_db_instance_list: %s\n" % e)
 
         return instance_list
 
@@ -38,7 +38,7 @@ class CloudDBConnector(NaverCloudConnector):
                 backup_list.append(instance)
 
         except ApiException as e:
-            print("Exception when calling V2Api->get_block_storage_instance_list: %s\n" % e)
+            print("Exception when calling V2Api->get_backup_list: %s\n" % e)
 
         return backup_list
 
@@ -53,6 +53,65 @@ class CloudDBConnector(NaverCloudConnector):
                 object_storage_backup_list.append(instance)
 
         except ApiException as e:
-            print("Exception when calling V2Api->get_block_storage_instance_list: %s\n" % e)
-
+            print("Exception when calling V2Api->get_object_storage_backup_list: %s\n" % e)
         return object_storage_backup_list
+
+    def list_product(self, cloudDBImageProductCode):
+        product_list = []
+        get_product_request = ncloud_clouddb.GetCloudDBProductListRequest(cloud_db_image_product_code=cloudDBImageProductCode)
+
+        try:
+            api_response = self.clouddb_client.get_cloud_db_product_list(get_product_request)
+
+            for instance in api_response.dms_file_list:
+                product_list.append(instance)
+
+        except ApiException as e:
+            print("Exception when calling V2Api->get_cloud_db_product_list: %s\n" % e)
+
+        return product_list
+
+    def list_img_product(self, dbKindCode):
+        img_product_list = []
+        get_img_product_request = ncloud_clouddb.GetCloudDBImageProductListRequest(db_kind_code=dbKindCode)
+
+        try:
+            api_response = self.clouddb_client.get_cloud_db_image_product_list(get_img_product_request)
+
+            for instance in api_response.product_list:
+                img_product_list.append(instance)
+
+        except ApiException as e:
+            print("Exception when calling V2Api->get_cloud_db_image_product_list: %s\n" % e)
+
+        return img_product_list
+
+    def list_dms_operation(self, requestNo):
+        operation_list = []
+        get_operation_request = ncloud_clouddb.GetDmsOperationRequest(request_no=requestNo)
+
+        try:
+            api_response = self.clouddb_client.get_dms_operation(get_operation_request)
+
+            for instance in api_response.cloud_db_instance_list:
+                operation_list.append(instance)
+
+        except ApiException as e:
+            print("Exception when calling V2Api->get_dms_operation: %s\n" % e)
+
+        return operation_list
+
+    def list_config_group(self, dbKindCode):
+        config_group_list = []
+        get_config_group_request = ncloud_clouddb.GetCloudDBConfigGroupListRequest(db_kind_code=dbKindCode)
+
+        try:
+            api_response = self.clouddb_client.get_cloud_db_config_group_list(get_config_group_request)
+
+            for instance in api_response.cloud_db_config_group_list:
+                config_group_list.append(instance)
+
+        except ApiException as e:
+            print("Exception when calling V2Api->get_cloud_db_config_group_list: %s\n" % e)
+
+        return config_group_list
