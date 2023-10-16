160879015563093fa675e6d527a07108d79767a3
Sehyun <brainmouse@naver.com>
2023-10-16 20:53:44 +0900

    feat : connector vpc 추가
    connector vpc추가
    
    Ref : #28

--- src/spaceone/inventory/libs/connector.py
+++ src/spaceone/inventory/libs/connector.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import ncloud_server
+import ncloud_vpc
 from ncloud_server.api.v2_api import V2Api
 import logging
 from spaceone.core.connector import BaseConnector
@@ -27,6 +28,7 @@ class NaverCloudConnector(BaseConnector):
 
         super().__init__(*args, **kwargs)
         self.client = None
+        self.vpc_client = None
         self.set_connect(kwargs['secret_data'])
 
     def set_connect(self, secret_data: object) -> object:
@@ -35,7 +37,15 @@ class NaverCloudConnector(BaseConnector):
         configuration.secret_key = secret_data['ncloud_secret_key']
         self.client = V2Api(ncloud_server.ApiClient(configuration))
 
+        configuration_vpc = ncloud_vpc.Configuration()
+        configuration_vpc.access_key = secret_data['ncloud_access_key_id']
+        configuration_vpc.secret_key = secret_data['ncloud_secret_key']
+        self.vpc_client = ncloud_vpc.V2Api(ncloud_vpc.ApiClient(configuration_vpc))
+
     def verify(self, **kwargs):
         if self.client is None:
+            self.set_connect(kwargs['secret_data'])
+            return "ACTIVE"
+        if self.vpc_client is None:
             self.set_connect(kwargs['secret_data'])
             return "ACTIVE"
