b0159e12af87d55b066c5fb078635360bc5e4153
Sehyun <brainmouse@naver.com>
2023-11-01 08:58:58 +0900

    feat : loadbalancer connector 기능 구현
    loadbalancer connector 기능 구현
    
    Ref : #57

--- src/spaceone/inventory/connector/networking/Loadbalancer_connector.py
+++ src/spaceone/inventory/connector/networking/Loadbalancer_connector.py
@@ -12,13 +12,13 @@ class LoadbalancerConnector(NaverCloudConnector):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def list_load_balanced_server_instance(self):
+    def list_load_balanced_server_instance(self, loadBalancerInstanceNo):
         load_balanced_server_instance_list = []
-        get_server_instance = ncloud_loadbalancer.GetLoadBalancedServerInstanceListRequest()
+        get_server_instance = ncloud_loadbalancer.GetLoadBalancedServerInstanceListRequest(load_balancer_instance_no=loadBalancerInstanceNo)
 
         try:
             api_response = self.loadbalancer_client.get_load_balanced_server_instance_list(get_server_instance)
-            for instance in api_response.load_balanced_server_instance_list:
+            for instance in api_response.server_instance_list:
                 load_balanced_server_instance_list.append(instance)
 
         except ApiException as e:
@@ -42,31 +42,31 @@ class LoadbalancerConnector(NaverCloudConnector):
         return load_balancer_instance_list
 
 
-    # def list_load_balancer_ssl_certificate(self):
-    #     load_balancer_ssl_certificate_list = []
-    #     get_ssl_certificate= ncloud_loadbalancer.GetLoadBalancerSslCertificateListRequest()
-    #
-    #     try:
-    #         api_response = self.loadbalancer_client.get_load_balancer_ssl_certificate_list(get_ssl_certificate)
-    #         for instance in api_response.load_balancer_ssl_certificate_list:
-    #             load_balancer_ssl_certificate_list.append(instance)
-    #
-    #     except ApiException as e:
-    #         print("Exception when calling V2Api->get_server_instance_list: %s\n" % e)
-    #
-    #     return load_balancer_ssl_certificate_list
-    #
-    #
-    # def list_load_balancer_target_server_instance(self):
-    #     load_balanced_target_server_instance_list = []
-    #     get_target_server_instance = ncloud_loadbalancer.GetLoadBalancerTargetServerInstanceListRequest()
-    #
-    #     try:
-    #         api_response = self.loadbalancer_client.get_load_balancer_target_server_instance_list(get_target_server_instance)
-    #         for instance in api_response.load_balancer_target_server_listance_list:
-    #             load_balanced_target_server_instance_list.append(instance)
-    #
-    #     except ApiException as e:
-    #         print("Exception when calling V2Api->get_server_instance_list: %s\n" % e)
-    #
-    #     return load_balanced_target_server_instance_list
+    def list_load_balancer_ssl_certificate(self):
+        ssl_certificate_list = []
+        get_ssl_certificate = ncloud_loadbalancer.GetLoadBalancerSslCertificateListRequest()
+
+        try:
+            api_response = self.loadbalancer_client.get_load_balancer_ssl_certificate_list(get_ssl_certificate)
+            for instance in api_response.ssl_certificate_list:
+                ssl_certificate_list.append(instance)
+
+        except ApiException as e:
+            print("Exception when calling V2Api->get_server_instance_list: %s\n" % e)
+
+        return ssl_certificate_list
+
+
+    def list_load_balancer_target_server_instance(self):
+        target_server_instance_list = []
+        get_target_server_instance = ncloud_loadbalancer.GetLoadBalancerTargetServerInstanceListRequest()
+
+        try:
+            api_response = self.loadbalancer_client.get_load_balancer_target_server_instance_list(get_target_server_instance)
+            for instance in api_response.server_instance_list:
+                target_server_instance_list.append(instance)
+
+        except ApiException as e:
+            print("Exception when calling V2Api->get_server_instance_list: %s\n" % e)
+
+        return target_server_instance_list
